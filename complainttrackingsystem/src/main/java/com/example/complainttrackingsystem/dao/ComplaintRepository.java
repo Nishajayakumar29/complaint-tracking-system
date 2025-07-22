7d6143b2a1f91dd56da26d6f58c74ad060c718b1
Nisha J <146064719+Nishajayakumar29@users.noreply.github.com>
2025-07-22 18:58:11 +0100

    Delete complainttrackingsystem directory

--- complainttrackingsystem/src/main/java/com/example/complainttrackingsystem/dao/ComplaintRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.complainttrackingsystem.dao;
-
-import com.example.complainttrackingsystem.model.Complaint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-    List<Complaint> findByUserId(Long userId); // To get complaints of a user
-}
--- complainttrackingsystem/src/main/java/com/example/complainttrackingsystem/service/OtpService.java
+++
@@ -1,9 +0,0 @@
-package com.example.complainttrackingsystem.service;
-
-
-public interface OtpService {
-    void saveOtp(String email, String otp);
-    boolean verifyOtp(String email, String otp);
-    String generateOtp(String email);  // <-- Add this method
-}
-
